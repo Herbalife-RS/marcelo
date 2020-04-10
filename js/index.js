435111da8abd5a3056edafc48da63923224de6e0
Herbalife-RS <62362821+Herbalife-RS@users.noreply.github.com>
2020-04-10 15:36:54 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,10 +2,47 @@
 $('#iWantToJoinInput').on('keyup', function(){
 	if($('#iWantToJoinInput').val().length > 2){
 		$('#submitBTN').prop('disabled', false)
+		let name = $('#iWantToJoinInput').val()
+		
+		$('#iWantToJoinInput').val(name.replace(name.charAt(0),name.charAt(0).toUpperCase()))
+		
+		
 	}else{
 		$('#submitBTN').prop('disabled', true)
 	}
 })
+
+
+$('#iWantToJoinInput').on('keydown',function(e){
+	let kc = [16,8,32,13,20]
+	let toReturn = false
+	
+	for(k of kc){
+		if(e.keyCode == k){
+			toReturn = true
+			break
+		}
+	}
+	
+	if(toReturn){
+		return
+	}
+	
+	
+	
+	if(Number.isInteger(parseInt(e.key.toString()))
+		|| e.key.toString() < 'a' 
+		|| e.key.toString() > 'z' ){
+				
+		e.preventDefault()
+		e.stopPropagation()
+		e.stopImmediatePropagation()
+				
+		return
+	}
+	
+})
+
 
 $('#iWantToJoin').on('blur', function(){
 	document.getElementById('iWantToJoin').style.display = 'none'
