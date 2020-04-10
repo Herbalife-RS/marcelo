0388970866f81d8de8fa7e9cfc7f1c5cb2953303
Herbalife-RS <62362821+Herbalife-RS@users.noreply.github.com>
2020-04-10 15:06:52 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 $('#iWantToJoinInput').on('keyup', function(){
 	if($('#iWantToJoinInput').val().length > 2){
 		$('#submitBTN').prop('disabled', false)
@@ -6,10 +7,14 @@ $('#iWantToJoinInput').on('keyup', function(){
 	}
 })
 
+$('#iWantToJoin').on('blur', function(){
+	document.getElementById('iWantToJoin').style.display = 'none'
+})
+
 
 $('.joinBTN').on('click',function(){
 	document.getElementById('iWantToJoin').style.display = 'block'
-
+	$("#iWantToJoinInput").focus()
 })
 $('#submitBTN').on('click',function(){
 	message = "Olá, me chamo " + $('#iWantToJoinInput').val() + ". Topo seu Desafio 5 Dias!"
